63e8961bcc073bc0555c184619964bc72ab2f175
gapigo <x.gabriel12@gmail.com>
2022-07-25 13:17:11 -0300

    Testing intArrayOf

--- src/main/kotlin/one/digitalinnovation/collections/TesteIntArrayOf.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteIntArrayOf.kt
@@ -1,5 +1,11 @@
 package one.digitalinnovation.collections
 
 fun main() {
+    val values = intArrayOf(5, 12, 13, 10, 12)
 
+    values.forEach { print("$it ") }
+
+    println()
+    values.sort()
+    values.forEach { print("$it ") }
 }
